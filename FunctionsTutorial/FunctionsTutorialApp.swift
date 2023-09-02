848842b69cbb63717b897f67dbfc9fdcbf1df7de
Jeremy Skrdlant <jeremy.skrdlant@nwktc.edu>
2023-09-01 20:00:44 -0600

    Initial Commit

---
+++ FunctionsTutorial/FunctionsTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunctionsTutorialApp.swift
+//  FunctionsTutorial
+//
+//  Created by Jeremy Skrdlant on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct FunctionsTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
